78f2bf8873a2386814bee35eb87933cde884852a
Edward Calderon <dev.edw.calderon@gmail.com>
2021-03-31 19:06:17 -0400

    Updated to use classpath to retrieve td_token.json file instead of relative string path.

--- src/main/java/com/portfoliowatch/config/TDAmeritradeConfig.java
+++ src/main/java/com/portfoliowatch/config/TDAmeritradeConfig.java
@@ -2,6 +2,7 @@ package com.portfoliowatch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,21 +10,20 @@ import java.io.IOException;
 @Configuration
 public class TDAmeritradeConfig {
 
-    private final File TOKEN_FILE =  new File("src/main/resources/td_token.json");
-
     @Bean
     public File getTokenFile() throws IOException {
-        if (TOKEN_FILE.exists()) {
-            if (TOKEN_FILE.isDirectory()) {
-                throw new IOException(TOKEN_FILE.getAbsolutePath() + " is not allowed to be a directory.");
+        File tokenFile = ResourceUtils.getFile("classpath:td_token.json");
+        if (tokenFile.exists()) {
+            if (tokenFile.isDirectory()) {
+                throw new IOException(tokenFile.getAbsolutePath() + " is not allowed to be a directory.");
             } else {
-                return TOKEN_FILE;
+                return tokenFile;
             }
         } else {
-            if (TOKEN_FILE.createNewFile()) {
-                return TOKEN_FILE;
+            if (tokenFile.createNewFile()) {
+                return tokenFile;
             } else {
-                throw new IOException(TOKEN_FILE.getAbsolutePath() + " could not be created.");
+                throw new IOException(tokenFile.getAbsolutePath() + " could not be created.");
             }
         }
     }
