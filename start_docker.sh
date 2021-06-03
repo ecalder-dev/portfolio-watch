9f163be67f7f32b1abc472efa58b6d6db190f1ae
Edward Calderon <dev.edw.calderon@gmail.com>
2021-06-03 19:32:33 -0400

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker build -t portfolio-watch .
 docker run -dp 9200:9200 --name portfolio-watch-container \
+--env DB_URL=$DB_URL --env TD_ACCT_ID=$TD_ACCT_ID \
 --env EMAIL_USER=$EMAIL_USER --env EMAIL_PASS=$EMAIL_PASS \
 --env KEY_STORE_LOC=$KEY_STORE_LOC --env KEY_STORE_LOC=$KEY_STORE_LOC \
 --env KEY_STORE_PASS=$KEY_STORE_PASS --env TD_REDIRECT=$TD_REDIRECT \
