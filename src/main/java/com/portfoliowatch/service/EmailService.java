5604f69ba70dfb623a7cd13f85b032081d3135de
Edward Calderon <dev.edw.calderon@gmail.com>
2022-12-07 22:37:49 +0900

    Spring version upgrade to 2.7.5

--- src/main/java/com/portfoliowatch/service/EmailService.java
+++ src/main/java/com/portfoliowatch/service/EmailService.java
@@ -1,11 +1,11 @@
 package com.portfoliowatch.service;
 
+import javax.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.mail.internet.MimeMessage;
 
 @Service
 @AllArgsConstructor
